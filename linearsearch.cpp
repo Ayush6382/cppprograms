f810f1fc5e0156f736cb4584881391499c52969d
Ayush Namdev <87568611+Ayush6382@users.noreply.github.com>
2021-08-20 11:27:03 +0530

    searching

---
+++ linearsearch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+  int main()
+  {
+  int n;
+  cin>>n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin>>a[i];
+  }
+  int key;
+  cin>>key;
+  for (int i = 0; i < n; i++)
+  {
+    if (a[i]==key)
+    {
+        cout<<"index is " <<i;
+        break;
+    }
+    
+  }
+  
+return 0;
+  }
