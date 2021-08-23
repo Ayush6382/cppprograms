ad894b1f4045dc4f205ed8fd2b22a3625cb698ad
Ayush Namdev <87568611+Ayush6382@users.noreply.github.com>
2021-08-23 22:36:08 +0530

    searching

---
+++ binarysearcharr.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;     // BINARY SEARCH ARRAY
+                          // IN THIS ARRAY MUST BE SORTED
+       int binarysearch(int arr[],int n,int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    int main()             // TIME REQUIRED WILL BE LESS
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i = 0; i < n; i++){
+            cin>>arr[i];
+         }  
+
+    
+    }
